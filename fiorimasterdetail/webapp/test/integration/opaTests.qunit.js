7eb886adf351bec2da5028fd9b7b70f03db6a698
lucila isernia <cursosapfiori2021@gmail.com>
2021-08-31 16:16:58 +0000

    primer commit Master Detail

---
+++ fiorimasterdetail/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,13 @@
+/* global QUnit */
+
+QUnit.config.autostart = false;
+
+sap.ui.getCore().attachInit(function() {
+	"use strict";
+
+	sap.ui.require([
+		"ns/fiorimasterdetail/test/integration/AllJourneys"
+	], function() {
+		QUnit.start();
+	});
+});
